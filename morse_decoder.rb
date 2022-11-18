a920378ba60a02447c55a3c7693cf4540d53a4a7
sonickmumba <kingellie.mumba@gmail.com>
2022-11-18 12:36:59 +0200

    Fix linter errors

--- morse_decoder.rb
+++ morse_decoder.rb
@@ -1,47 +1,47 @@
 # Function to decode into a character
-def decode_character(string)
-morse_code_hash = {
-  '.-' => 'A',
-  '-...' => 'B',
-  '-.-.' => 'C',
-  '-..' => 'D',
-  '.' => 'E',
-  '..-.' => 'F',
-  '--.' => 'G',
-  '....' => 'H',
-  '..' => 'I',
-  '.---' => 'J',
-  '-.-' => 'K',
-  '.-..' => 'L',
-  '--' => 'M',
-  '-.' => 'N',
-  '---' => 'O',
-  '.--.' => 'P',
-  '--.-' => 'Q',
-  '.-.' => 'R',
-  '...' => 'S',
-  '-' => 'T',
-  '..-' => 'U',
-  '...-' => 'V',
-  '.--' => 'W',
-  '-..-' => 'X',
-  '-.--' => 'Y',
-  '--..' => 'Z'
-}
-morse_code_hash[string]
+def decode_char(str)
+  morse_code_hash = {
+    '.-' => 'A',
+    '-...' => 'B',
+    '-.-.' => 'C',
+    '-..' => 'D',
+    '.' => 'E',
+    '..-.' => 'F',
+    '--.' => 'G',
+    '....' => 'H',
+    '..' => 'I',
+    '.---' => 'J',
+    '-.-' => 'K',
+    '.-..' => 'L',
+    '--' => 'M',
+    '-.' => 'N',
+    '---' => 'O',
+    '.--.' => 'P',
+    '--.-' => 'Q',
+    '.-.' => 'R',
+    '...' => 'S',
+    '-' => 'T',
+    '..-' => 'U',
+    '...-' => 'V',
+    '.--' => 'W',
+    '-..-' => 'X',
+    '-.--' => 'Y',
+    '--..' => 'Z'
+  }
+  morse_code_hash[string]
 end
 
-puts decode_character('.')
+puts decode_char('.')
 
 # Function to decode into a word
 def decode_to_word(string)
   string_arr = string.split
   final_string = ''
-  string_arr.each { |char| final_string += decode_character(char) }
+  string_arr.each { |char| final_string += decode_char(char) }
   "#{final_string}"
 end
 
-puts decode_to_word("-- -.--")
+puts decode_to_word('-- -.--')
 
 # Function to decode into a sentence
 def decoder(str)
